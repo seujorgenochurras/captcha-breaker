00a621b6996e249a464d3a2d0173a080d98b2026
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-02-12 14:25:25 -0300

    Added a way to log I'll use this latter for the token

--- src/main/java/org/jhey/Main.java
+++ src/main/java/org/jhey/Main.java
@@ -1,15 +1,16 @@
 package org.jhey;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.By;
-import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v96.network.Network;
+import org.openqa.selenium.devtools.v109.network.Network;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,54 +23,55 @@ public class Main {
    static ChromeDriverService service;
    public static void main(String[] args) throws InterruptedException, IOException {
       System.setProperty("webdriver.chrome.driver", "src/drive/chromedriver.exe");
-
-
       ChromeOptions options = new ChromeOptions();
-      Map<String, Object> perfLogPrefs = new HashMap<>();
-      perfLogPrefs.put("enableNetwork", true);
-      options.setExperimentalOption("perfLoggingPrefs", perfLogPrefs);
-         options.addArguments("--disable-extensions");
-      options.addArguments("--disable-popup-blocking");
-      options.addArguments("--disable-translate");
-      options.addArguments("--disable-notifications");
-     // options.setExperimentalOption("debuggerAddress", "localhost:9222");
 
       LoggingPreferences logPrefs = new LoggingPreferences();
       logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-
       options.setCapability("goog:loggingPrefs", logPrefs);
+      options.addArguments("--disable-extensions");
+      options.addArguments("--disable-popup-blocking");
+      options.addArguments("--incognito");
+      options.addArguments("--start-maximized");
+      options.addArguments("--headless");
+
       createService();
       driver = new ChromeDriver(options);
       initAndEnableChromeDevtools();
-       chromeDevTools.addListener(Network.loadingFinished(), requestIntercepted->{
-         System.out.println("Body: " + chromeDevTools.send(Network.getResponseBody(requestIntercepted.getRequestId())).getBody());
-      });
-
 
       driver.get("https://nsa.cps.sp.gov.br/");
+      fillAccountInfo();
+      breakCaptcha();
+      attemptLogin();
+      chromeDevTools.send(Network.getAllCookies()).stream().filter(cookie -> cookie.getName().equals("NSA_OnLine_SessionId"))
+              .forEach(cookie -> {
+                 System.out.println("Name : " + cookie.getName());
+                 System.out.println("Value : " +  cookie.getValue());
+              });
 
-
-      //driver.navigate().to("devtools://devtools/bundled/devtools_app.html?ws=localhost:9222/devtools/page/1");
-
-//      ((JavascriptExecutor) driver).executeScript("window.open()");
-//      ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//
-//     // driver.switchTo().window(tabs.get(1));
-//
-//      // Access the Network Tab in the DevTools panel
-//      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//      wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Network']")));
-//      driver.findElement(By.xpath("//div[text()='Network']")).click();
-//      wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Doc']")));
-
-
-      driver.findElement(By.xpath("//*[@id=\"ctrlGoogleReCaptcha\"]/div/div/div/iframe")).click();
-      Thread.sleep(10000);
-
-      driver.quit();
+   }
+      static void attemptLogin(){
+         WebElement inputButton = driver.findElement(By.xpath("//*[@id=\"btnEntrar\"]"));
+         inputButton.click();
       }
+      static void breakCaptcha() throws InterruptedException {
+         driver.findElement(By.xpath("//*[@id=\"ctrlGoogleReCaptcha\"]/div/div/div/iframe")).click();
+         Thread.sleep(10000);
+      }
+      static void fillAccountInfo(){
+         Dotenv dotenv = Dotenv.load();
+         String etecId = dotenv.get("ETEC_ID");
+         String studentId = dotenv.get("STUDENT_ID");
+         String studentPass = dotenv.get("STUDENT_PASS");
 
+         WebElement inputEtecID = driver.findElement(By.xpath("//*[@id=\"txtCod\"]"));
+         WebElement inputStudentID = driver.findElement(By.xpath("//*[@id=\"txtlogin\"]"));
+         WebElement inputStudentPass = driver.findElement(By.xpath("//*[@id=\"txtSenha\"]"));
 
+         inputEtecID.sendKeys(etecId);
+         inputStudentID.sendKeys(studentId);
+         inputStudentPass.sendKeys(studentPass);
+
+   }
       static void createService() throws IOException {
          service = new ChromeDriverService.Builder()
                  .usingDriverExecutable(new File("src\\drive\\chromedriver.exe"))
@@ -77,17 +79,10 @@ public class Main {
                  .build();
          service.start();
       }
-      static void printLog(String type){
-         List<LogEntry> logEntries = driver.manage().logs().get(type).getAll();
-         logEntries.forEach(logEntry -> System.out.println(logEntry.toString()));
-      }
-
 
       static void initAndEnableChromeDevtools() {
          chromeDevTools = driver.getDevTools();
          chromeDevTools.createSession();
          chromeDevTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-
-
       }
    }

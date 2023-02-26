901ab9e95a1cbe324b63d6ed5d5e7bd98e2c7189
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-02-26 16:12:26 -0300

    Deleted unused file

--- src/main/java/org/jhey/Main.java
+++ src/main/java/org/jhey/Main.java
@@ -1,27 +1,6 @@
 package org.jhey;
 
-
-import org.jhey.captcha_breaker.stt.html.elements.captcha.Captcha;
-import org.jhey.captcha_breaker.stt.selenium.captcha.CaptchaFinder;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 public class Main {
-   static ChromeDriver driver;
    public static void main(String[] args) {
-      System.setProperty("webdriver.chrome.driver", "src/drive/chromedriver.exe");
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--disable-extensions");
-      options.addArguments("--disable-popup-blocking");
-      options.addArguments("--incognito");
-      options.addArguments("--start-maximized");
-
-      driver = new ChromeDriver(options);
-
-      driver.get("https://www.google.com/recaptcha/api2/demo");
-
-      Captcha captcha = CaptchaFinder.findCaptchaElement(driver);
-      captcha.solveCaptcha();
-
    }
 }
